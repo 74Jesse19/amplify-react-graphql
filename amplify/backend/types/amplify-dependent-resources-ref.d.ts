aaee02e82297ca103b7a818d1c6e33ef6898d6f1
JDW0426 <jesse.williams@daugherty.com>
2024-03-28 14:54:32 -0500

    fixed issues

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqlc37eeb2a": {
       "AppClientID": "string",
